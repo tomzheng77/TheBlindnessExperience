a6e5c1ea33d1897a56db6be6b7bff8483228160c
tomzheng77 <tomzheng77@live.com>
2019-11-28 10:36:17 +1100

    train moves

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject platform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            player.transform.parent = platform.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            player.transform.parent = null;
+        }
+    }
+
+}
---
+++ Assets/TrainFollowPath.cs
@@ -0,0 +1,64 @@
+﻿using PathCreation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainFollowPath : MonoBehaviour
+{
+
+    public PathCreator pathCreator;
+    public EndOfPathInstruction endOfPathInstruction;
+    public float speed = 0;
+    float distanceTravelled;
+    public GameObject player;
+    bool playerInside = false;
+
+    void Start()
+    {
+        if (pathCreator != null)
+        {
+            // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
+            pathCreator.pathUpdated += OnPathChanged;
+        }
+    }
+
+    void Update()
+    {
+        if (playerInside)
+        {
+            speed += Time.deltaTime * 3;
+            speed = Mathf.Min(speed, 20);
+        }
+        if (pathCreator != null)
+        {
+            distanceTravelled += speed * Time.deltaTime;
+            transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+            transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+            transform.Rotate(270, 90, 0);
+        }
+    }
+
+    // If the path changes during the game, update the distance travelled so that the follower's position on the new path
+    // is as close as possible to its position on the old path
+    void OnPathChanged()
+    {
+        distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInside = false;
+        }
+    }
+
+}
