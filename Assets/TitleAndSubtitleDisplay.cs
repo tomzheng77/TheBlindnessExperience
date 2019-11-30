1269f2b7ede8c9a8b36004b3aeb64eac6617a275
tomzheng77 <tomzheng77@live.com>
2019-12-01 04:57:19 +1100

    label system

--- Assets/TitleAndSubtitleDisplay.cs
+++ Assets/TitleAndSubtitleDisplay.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Animator))]
 public class TitleAndSubtitleDisplay : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -11,48 +12,31 @@ public class TitleAndSubtitleDisplay : MonoBehaviour
     public GameObject titleImage;
     public GameObject subtitle;
     public GameObject subtitleImage;
-    private int ticks = 0;
+
+    private Animator animator;
 
     void Start()
     {
-        title.SetActive(false);
-        subtitle.SetActive(false);
-        titleImage.SetActive(false);
-        subtitleImage.SetActive(false);
+        animator = GetComponent<Animator>();
+        animator.speed = 0;
+        animator.playbackTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        ticks -= 1;
-        if (ticks < 0) ticks = 0;
-        if (ticks <= 0)
+        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.99)
         {
-            title.SetActive(false);
-            titleImage.SetActive(false);
-            subtitle.SetActive(false);
-            subtitleImage.SetActive(false);
-        }
-        else
-        {
-            float alpha = ticks / 60f;
-            if (alpha > 1f) alpha = 1f;
-            if (title.activeSelf) title.GetComponent<Text>().color = new Color(0, 0, 0, alpha);
-            if (subtitle.activeSelf) subtitle.GetComponent<Text>().color = new Color(0, 0, 0, alpha);
-            if (titleImage.activeSelf) titleImage.GetComponent<Image>().color = new Color(.5f, .5f, .5f, alpha/2);
-            if (subtitleImage.activeSelf) subtitleImage.GetComponent<Image>().color = new Color(.5f, .5f, .5f, alpha/2);
+            animator.speed = 0;
         }
     }
 
     void ShowMessage(string[] strs)
     {
-        ticks = 480;
-        title.SetActive(true);
-        subtitle.SetActive(true);
-        titleImage.SetActive(true);
-        subtitleImage.SetActive(true);
         title.GetComponent<Text>().text = strs[0];
         subtitle.GetComponent<Text>().text = strs[1];
+        animator.speed = 1;
+        animator.playbackTime = 0;
     }
 
 }
